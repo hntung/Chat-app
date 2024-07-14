d5bc114b4d63b8e1da5e97595900b9a6261c1e5e
hntung <hongoctungii@gmail.com>
2024-07-14 12:35:50 +0700

    Install Tailwind Css

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <main className='font-bold text-2x1 text-red-600'>
+        React App
+    </main>
+  );
+}
+
+export default App;
