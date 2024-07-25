3c94128930f25779215fb1997dd89419d67424de
hntung <hongoctungii@gmail.com>
2024-07-25 09:10:44 +0700

    SearchUser component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,46 +1,60 @@
 import axios from 'axios'
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Outlet, useNavigate } from 'react-router-dom'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 import { logout, setUser } from '../redux/userSlice'
 import Sidebar from '../components/Sidebar'
+import logo from '../assets/logo.png'
 
 function Home() {
   const user = useSelector(state => state.user)
   const dispatch = useDispatch()
   const navigate = useNavigate()
-  console.log(user)
-  const fecthUserDetails = async()=>{
-    try {
-        const URL = `${process.env.REACT_APP_BACKEND_URL}/api/user-details`
-        const response = await axios({
-            url : URL,
-            withCredentials : true
-        })
+  const location = useLocation()
 
-        dispatch(setUser(response.data.data))
-        if(response.data.logout){
-          dispatch(logout())
-          navigate('/email')
-        }
-        console.log("current user details: ",response)
+  const fecthUserDetails = async () => {
+    try {
+      const URL = `${process.env.REACT_APP_BACKEND_URL}/api/user-details`
+      const response = await axios({
+        url: URL,
+        withCredentials: true
+      })
+
+      dispatch(setUser(response.data.data))
+      if (response.data.data.logout) {
+        dispatch(logout())
+        navigate('/email')
+      }
+      console.log("current user details: ", response)
     } catch (error) {
-        console.log(error)
+      console.log(error)
     }
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     fecthUserDetails()
-  },[])
+  }, [])
 
+  const basePath = location.pathname === '/'
   return (
     <div className='grid lg:grid-cols-[300px,1fr] h-screen max-h-screen'>
-        <section className='bg-white'>
-          <Sidebar />
-        </section>
-        <section>
-            <Outlet />
-        </section>
+      <section className={`bg-white ${!basePath && "hidden"} lg:block`}>
+        <Sidebar />
+      </section>
+      <section className={`${basePath && "hidden"}`}>
+        <Outlet />
+      </section>
+
+      <div className={`justify-center items-center flex-col gap-2 hidden ${!basePath ? "hidden" : "lg:flex" }`}>
+        <div>
+          <img
+            src={logo}
+            width={250}
+            alt='logo'
+          />
+        </div>
+        <p className='text-lg mt-2 text-slate-500'>Select user to send message</p>
+      </div>
     </div>
   )
 }
